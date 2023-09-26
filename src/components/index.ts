6dae0c344ac5713a9c9df15bb53d14c41ce5216f
mohamedagina <mohamedagina001@gmail.com>
2023-09-26 21:10:01 +0300

    feat: Add sidebar

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { SideBar } from './side-bar/SideBar';
+export { ProfilePicPlaceholder } from './profile-pic-placeholder/ProfilePicPlaceholder';
---
+++ src/components/side-bar/SideBar.tsx
@@ -0,0 +1,98 @@
+import { useState, useEffect, useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Tooltip } from 'antd';
+import {
+  GoHome,
+  GoPeople,
+  GoChevronLeft,
+  GoShareAndroid,
+  GoHeart
+} from 'react-icons/go';
+import { FiMenu } from 'react-icons/fi';
+import { LuCalendarCheck, LuFileText } from 'react-icons/lu';
+import { PiNotebook } from 'react-icons/pi';
+
+import './SideBar.css';
+import { ProfilePicPlaceholder } from '..';
+
+const navLinks = [
+  { path: '/', title: 'Dashboard', icon: <GoHome /> },
+  { path: '/candidates', title: 'Candidate Bank', icon: <GoPeople /> },
+  {
+    path: '/interviews',
+    title: 'Interview Manager',
+    icon: <LuCalendarCheck />
+  },
+  {
+    path: '/opportunities',
+    title: 'Opportunity Hub',
+    icon: <GoShareAndroid />
+  },
+  { path: '/documents', title: 'Documents', icon: <LuFileText /> },
+  { path: '/contacts', title: 'Contacts', icon: <PiNotebook /> },
+  { path: '/favorite', title: 'Favorite', icon: <GoHeart /> }
+];
+
+const user = { name: 'mohamed agina' };
+
+export const SideBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const sidebarEl = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const sidebar = sidebarEl.current;
+    if (!sidebar) return;
+
+    const handleClose = (e: MouseEvent) => {
+      if (!sidebar.contains(e.target as Node)) setIsExpanded(false);
+    };
+
+    document.addEventListener('click', handleClose, true);
+
+    return () => document.removeEventListener('click', handleClose);
+  }, []);
+
+  return (
+    <>
+      <header className="mobile-header">
+        {' '}
+        <Tooltip placement="left" title="Expand">
+          <button className="burger-btn" onClick={() => setIsExpanded(true)}>
+            <FiMenu />
+          </button>
+        </Tooltip>
+      </header>
+
+      <div className={`overlay-wrapper ${isExpanded ? 'expanded' : ''}`}>
+        <aside ref={sidebarEl} className="app-sidebar">
+          <img className="app-logo" src="./app-logo.png" alt="app logo" />
+
+          <nav>
+            <ul className="nav-links">
+              {navLinks.map(link => (
+                <li key={link.title}>
+                  <NavLink to={link.path}>
+                    <span className="link-icon">{link.icon}</span> {link.title}
+                  </NavLink>
+                </li>
+              ))}
+
+              <li>
+                <button className="back-btn">
+                  <span className="link-icon">
+                    <GoChevronLeft />
+                  </span>{' '}
+                  Go Back
+                </button>
+              </li>
+            </ul>
+          </nav>
+          <div className="user-identity">
+            <ProfilePicPlaceholder username={user.name} />
+            {user.name}
+          </div>
+        </aside>
+      </div>
+    </>
+  );
+};
