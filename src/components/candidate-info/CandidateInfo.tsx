da67b44c5ba0047a79845562db28e862e561970e
mohamedagina <mohamedagina001@gmail.com>
2023-09-27 20:05:55 +0300

    feat: Implement candidate selection

--- src/components/candidate-info/CandidateInfo.tsx
+++ src/components/candidate-info/CandidateInfo.tsx
@@ -1,13 +1,26 @@
 import { Checkbox } from 'antd';
 import { Candidate } from '../../models';
-import './CandidateInfo.css';
 import { ProfilePicPlaceholder } from '..';
 import { Link } from 'react-router-dom';
+import { useCandidatesContext } from '../../hooks';
+import { SharedObj } from '../../context/candidates-context';
+import './CandidateInfo.css';
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 export const CandidateInfo = ({ candidate }: { candidate: Candidate }) => {
+  const { isSelected, selectCandidates, unSelectCandidates } =
+    useCandidatesContext() as SharedObj;
+
+  const handleCheckChange = (e: CheckboxChangeEvent) => {
+    if (e.target.checked) selectCandidates([candidate.id]);
+    else unSelectCandidates([candidate.id]);
+  };
   return (
     <div className="candidate">
-      <Checkbox />
+      <Checkbox
+        checked={isSelected(candidate.id)}
+        onChange={handleCheckChange}
+      />
       <ProfilePicPlaceholder className="xl" username={candidate.name} />
       <div className="candidate-info">
         <h2 className="semi-bold md">
